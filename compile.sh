5d7465cb143543f87e1c7a084065cb3e48caa08a
pbolotin <pavel.bolotin@gmail.com>
2020-07-14 15:19:46 +0700

    Change compile.sh to allow gdb debug process

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-gcc -c lab_02/lab_02.c -o lab_02/lab_02.o
-gcc -c sc_test.c -o sc_test.o
-gcc lab_02/lab_02.o sc_test.o -o sc_test.out
+gcc -c -g lab_02/lab_02.c -o lab_02/lab_02.o
+gcc -c -g sc_test.c -o sc_test.o
+gcc -g lab_02/lab_02.o sc_test.o -o sc_test.out
