8984aab337f01a6f883e9c1d155f8db768016b2d
pbolotin <pavel.bolotin@gmail.com>
2020-08-29 22:08:50 +0700

    Create lab_03/lab_03.sh which does task for lab_03 point 3

---
+++ lab_03/lab_03.sh
@@ -0,0 +1,11 @@
+#clear screen
+echo -e "\E[H\E[2J"
+
+#move 5,10 and print red on black
+echo -e "\E[5;10H\E[40m\E[31;1mPavel"
+
+#move 6,8 and print red on black
+echo -e "\E[6;8H\E[47;1m\E[32;1mZP-81"
+
+#move 10,1 and default
+echo -e "\E[10;1H\E[0m"
