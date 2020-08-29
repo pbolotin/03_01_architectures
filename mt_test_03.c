2c28ebd56a5c3015cc28565e0cd1e430f657c2fb
pbolotin <pavel.bolotin@gmail.com>
2020-08-30 00:06:32 +0700

    Add test_mt_clrscr function in mt_test_03.c

--- mt_test_03.c
+++ mt_test_03.c
@@ -2,11 +2,25 @@
 #include <unistd.h>
 #include "lab_03/lab_03.h"
 
+int test_mt_clrscr(void) {
+    printf("test mt_clrscr: screen will be cleared after 5 second:\n");
+    printf("5\n");
+    sleep(1);
+    printf("4\n");
+    sleep(1);
+    printf("3\n");
+    sleep(1);
+    printf("2\n");
+    sleep(1);
+    printf("1\n");
+    sleep(1);
+    mt_clrscr();
+    return 0;
+}
+
 int main(void) {
     int sizeX, sizeY;
-    printf("test mt_clrscr: screen will be cleared after 5 second:\n");
-    sleep(5);
-    mt_clrscr();
+    test_mt_clrscr();
     mt_gotoXY(10, 10);
     mt_getscreensize(&sizeX, &sizeY);
     mt_setfgcolor(GREEN);
