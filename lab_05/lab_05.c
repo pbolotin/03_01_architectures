d4075a015478ad29250e2f9855eb6c42f575632a
pbolotin <pavel.bolotin@gmail.com>
2020-10-07 16:40:41 +0700

    Lab_05 Add template rk_mytermragime and simple test

--- lab_05/lab_05.c
+++ lab_05/lab_05.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <termios.h>
 #include <sys/ioctl.h>
 #include "lab_05.h"
 
@@ -35,6 +36,7 @@ int rk_mytermrestore(void)  {
 }
 
 int rk_mytermregime(int regime, int vtime, int vmin, int echo, int sigint)  {
+    if(vtime < 0 || vmin < 0) return -1;
     /*
     struct termios{
         tcflag_t c_iflag;
@@ -43,6 +45,70 @@ int rk_mytermregime(int regime, int vtime, int vmin, int echo, int sigint)  {
         tcflag_t c_cflag;
         tcflag_t c_cc[NCCS];
     }*/
-    
+    if(regime & ICANON) {
+        printf("Set ICANON flag\n");
+    } else {
+        printf("Unset ICANON flag\n");
+    }
+    if(echo & ECHO) {
+        printf("Set ECHO flag\n");
+    } else {
+        printf("Unset ECHO flag\n");
+    }
+    if(sigint & ISIG) {
+        printf("Set ISIG flag\n");
+    } else {
+        printf("Unset ISIG flag\n");
+    }
+    /*
+    MIN == 0, TIME == 0 (polling read)
+    If data is available, read(2) returns immediately,
+    with the lesser of the number of bytes available,
+    or the number of bytes requested.
+    If no data is available, read(2) returns 0.
+    */
+    if(vmin == 0 && vtime == 0) {
+        printf("Set polling read\n");
+    }
+    /*
+    MIN > 0, TIME == 0 (blocking read)
+    read(2) blocks until MIN bytes are available,
+    and returns up to the number of bytes requested.
+    */
+    else if(vmin > 0 && vtime == 0) {
+        printf("Set blocking read\n");
+    }
+    /*
+    MIN == 0, TIME > 0 (read with timeout)
+    TIME specifies the limit for a timer in tenths of a second.
+    The timer is started when read(2) is called.
+    read(2) returns either when at least one byte of data is available,
+    or when the timer expires.
+    If the timer expires without any input becoming available,
+    read(2) returns 0.  If data is already available
+    at the time of the call to read(2),
+    the call behaves as though the data was received immediately after the call.
+    */ 
+    else if(vmin == 0 && vtime > 0) {
+        printf("Set read with timeout\n");
+    }
+    /* MIN > 0, TIME > 0 (read with interbyte timeout)
+    TIME specifies the limit for a timer in tenths of a second.
+    Once an initial byte of input becomes available,
+    the timer is restarted after each further byte is received.
+    read(2) returns when any of the following conditions is met:
+    MIN bytes have been received.
+    The interbyte timer expires.
+    The number of bytes requested by read(2) has been received.
+    (POSIX does not specify this termination condition,
+    and on some other implementations read(2) does not return in this case.)
+    Because the timer is started only after the initial byte becomes available,
+    at least one byte will be read.
+    If data is already available at the time of the call to read(2),
+    the call behaves as though the data was received immediately after the call.
+    */
+    else if(vmin > 0 && vtime > 0) {
+        printf("Set read with interbyte timeout\n");
+    }
     return 0;
 }
--- rk_test_05.c
+++ rk_test_05.c
@@ -5,5 +5,6 @@ int main() {
     printf("rk_test_05");
     enum keys key_place;
     rk_readkey(&key_place);
+    rk_mytermregime(0, 0, 0, 0, 0);
     return 0;
 }
