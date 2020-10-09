7b9a7af65b5ebd21e5247d59ddf32e7450ee7359
pbolotin <pavel.bolotin@gmail.com>
2020-10-09 14:49:42 +0700

    Lab_05 checking of vmin vtime in rk_mytermregime now only in case of not ICANNON flag trying to set

--- lab_05/lab_05.c
+++ lab_05/lab_05.c
@@ -36,8 +36,6 @@ int rk_mytermrestore(void)  {
 }
 
 int rk_mytermregime(int regime, int vtime, int vmin, int echo, int sigint)  {
-    if(vtime < 0 || vmin < 0) return -1;
-    
     /*
     struct termios{
         tcflag_t c_iflag;
@@ -54,8 +52,70 @@ int rk_mytermregime(int regime, int vtime, int vmin, int echo, int sigint)  {
         printf("Set ICANON flag\n");
         myTermStruct.c_lflag |= ICANON;
     } else {
+        if(vtime < 0 || vmin < 0) {
+            perror("Error, vtime or vmin are incorrect\n");
+            return -1;
+        }
         printf("Unset ICANON flag\n");
         myTermStruct.c_lflag &= ~ICANON;
+            /*
+        MIN == 0, TIME == 0 (polling read)
+        If data is available, read(2) returns immediately,
+        with the lesser of the number of bytes available,
+        or the number of bytes requested.
+        If no data is available, read(2) returns 0.
+        */
+        if(vmin == 0 && vtime == 0) {
+            printf("Set polling read\n");
+            myTermStruct.c_cc[VMIN] = vmin;
+            myTermStruct.c_cc[VTIME] = vtime;
+        }
+        /*
+        MIN > 0, TIME == 0 (blocking read)
+        read(2) blocks until MIN bytes are available,
+        and returns up to the number of bytes requested.
+        */
+        else if(vmin > 0 && vtime == 0) {
+            printf("Set blocking read\n");
+            myTermStruct.c_cc[VMIN] = vmin;
+            myTermStruct.c_cc[VTIME] = vtime;
+        }
+        /*
+        MIN == 0, TIME > 0 (read with timeout)
+        TIME specifies the limit for a timer in tenths of a second.
+        The timer is started when read(2) is called.
+        read(2) returns either when at least one byte of data is available,
+        or when the timer expires.
+        If the timer expires without any input becoming available,
+        read(2) returns 0.  If data is already available
+        at the time of the call to read(2),
+        the call behaves as though the data was received immediately after the call.
+        */ 
+        else if(vmin == 0 && vtime > 0) {
+            printf("Set read with timeout\n");
+            myTermStruct.c_cc[VMIN] = vmin;
+            myTermStruct.c_cc[VTIME] = vtime;
+        }
+        /* MIN > 0, TIME > 0 (read with interbyte timeout)
+        TIME specifies the limit for a timer in tenths of a second.
+        Once an initial byte of input becomes available,
+        the timer is restarted after each further byte is received.
+        read(2) returns when any of the following conditions is met:
+        MIN bytes have been received.
+        The interbyte timer expires.
+        The number of bytes requested by read(2) has been received.
+        (POSIX does not specify this termination condition,
+        and on some other implementations read(2) does not return in this case.)
+        Because the timer is started only after the initial byte becomes available,
+        at least one byte will be read.
+        If data is already available at the time of the call to read(2),
+        the call behaves as though the data was received immediately after the call.
+        */
+        else if(vmin > 0 && vtime > 0) {
+            printf("Set read with interbyte timeout\n");
+            myTermStruct.c_cc[VMIN] = vmin;
+            myTermStruct.c_cc[VTIME] = vtime;
+        }
     }
     if(echo & ECHO) {
         printf("Set ECHO flag\n");
@@ -70,64 +130,6 @@ int rk_mytermregime(int regime, int vtime, int vmin, int echo, int sigint)  {
     } else {
         printf("Unset ISIG flag\n");
         myTermStruct.c_lflag &= ~ISIG;
-    }
-    /*
-    MIN == 0, TIME == 0 (polling read)
-    If data is available, read(2) returns immediately,
-    with the lesser of the number of bytes available,
-    or the number of bytes requested.
-    If no data is available, read(2) returns 0.
-    */
-    if(vmin == 0 && vtime == 0) {
-        printf("Set polling read\n");
-        myTermStruct.c_cc[VMIN] = vmin;
-        myTermStruct.c_cc[VTIME] = vtime;
-    }
-    /*
-    MIN > 0, TIME == 0 (blocking read)
-    read(2) blocks until MIN bytes are available,
-    and returns up to the number of bytes requested.
-    */
-    else if(vmin > 0 && vtime == 0) {
-        printf("Set blocking read\n");
-        myTermStruct.c_cc[VMIN] = vmin;
-        myTermStruct.c_cc[VTIME] = vtime;
-    }
-    /*
-    MIN == 0, TIME > 0 (read with timeout)
-    TIME specifies the limit for a timer in tenths of a second.
-    The timer is started when read(2) is called.
-    read(2) returns either when at least one byte of data is available,
-    or when the timer expires.
-    If the timer expires without any input becoming available,
-    read(2) returns 0.  If data is already available
-    at the time of the call to read(2),
-    the call behaves as though the data was received immediately after the call.
-    */ 
-    else if(vmin == 0 && vtime > 0) {
-        printf("Set read with timeout\n");
-        myTermStruct.c_cc[VMIN] = vmin;
-        myTermStruct.c_cc[VTIME] = vtime;
-    }
-    /* MIN > 0, TIME > 0 (read with interbyte timeout)
-    TIME specifies the limit for a timer in tenths of a second.
-    Once an initial byte of input becomes available,
-    the timer is restarted after each further byte is received.
-    read(2) returns when any of the following conditions is met:
-    MIN bytes have been received.
-    The interbyte timer expires.
-    The number of bytes requested by read(2) has been received.
-    (POSIX does not specify this termination condition,
-    and on some other implementations read(2) does not return in this case.)
-    Because the timer is started only after the initial byte becomes available,
-    at least one byte will be read.
-    If data is already available at the time of the call to read(2),
-    the call behaves as though the data was received immediately after the call.
-    */
-    else if(vmin > 0 && vtime > 0) {
-        printf("Set read with interbyte timeout\n");
-        myTermStruct.c_cc[VMIN] = vmin;
-        myTermStruct.c_cc[VTIME] = vtime;
     }
     /*Set new value of termios*/
     tcsetattr(0, TCSADRAIN, &myTermStruct);
