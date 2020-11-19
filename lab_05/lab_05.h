926d9616a6afcf3518ebc6b7bef9f05f9ad75825
pbolotin <pavel.bolotin@gmail.com>
2020-11-19 09:45:14 +0700

    Lab05 add keys like KEY_RIGHT, KEY_DOWN into enum for rkreadkey

--- lab_05/lab_05.h
+++ lab_05/lab_05.h
@@ -3,7 +3,14 @@
 
 enum keys {
     KEY_UP,
-    KEY_DOWN
+    KEY_DOWN,
+    KEY_RIGHT,
+    KEY_LEFT,
+    KEY_S,
+    KEY_L,
+    KEY_Q,
+    KEY_F5,
+    KEY_F6
 };
 
 int rk_readkey(enum keys * key);
