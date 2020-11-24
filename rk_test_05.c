5c74eaad690d0d9bd1f21dec77092381aaf22044
pbolotin <pavel.bolotin@gmail.com>
2020-11-24 18:31:53 +0700

    Lab05: change rk_test_05 to demonstrate reading of symbols using uncanonical regime of terminal with reading timeout

--- rk_test_05.c
+++ rk_test_05.c
@@ -11,10 +11,50 @@ int main() {
 
     rk_mytermsave();
     
-    rk_mytermregime(~ICANON, 0, 1, ECHO, ISIG);
+    //rk_mytermregime(~ICANON, 0, 1, ~ECHO, ISIG);
+    rk_mytermregime(~ICANON, 1, 0, ~ECHO, ISIG);
     //rk_mytermregime(ICANON, 0, 0, ~ECHO, ISIG);
-    rk_readkey(&key_place);
-    
+    int flag_to_quit = 0;
+    while(!flag_to_quit) {
+        if(rk_readkey(&key_place) > 0) {
+            switch(key_place) {
+                case KEY_s:
+                case KEY_S:
+                    printf("S\n");
+                    break;
+                case KEY_l:
+                case KEY_L:
+                    printf("L\n");
+                    break;
+                case KEY_q:
+                case KEY_Q:
+                    printf("Q\n");
+                    flag_to_quit = 1;
+                    break;
+                case KEY_UP:
+                    printf("UP\n");
+                    break;
+                case KEY_DOWN:
+                    printf("DOWN\n");
+                    break;
+                case KEY_RIGHT:
+                    printf("RIGHT\n");
+                    break;
+                case KEY_LEFT:
+                    printf("LEFT\n");
+                    break;
+                case KEY_F5:
+                    printf("F5\n");
+                    break;
+                case KEY_F6:
+                    printf("F6\n");
+                    break;
+                case UNKNOWN_KEY:
+                    printf("UNKNOWN\n");
+                    break;
+            }
+        }
+    }
     rk_mytermrestore();
     return 0;
 }
